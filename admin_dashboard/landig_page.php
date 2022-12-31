a2190eaf44abb65a603318c9a5745292beecbd14
unknown <talab.yaseen@gmail.com>
2022-12-31 18:28:45 +0300

    2nd comment

--- admin_dashboard/landig_page.php
+++ admin_dashboard/landig_page.php
@@ -8,24 +8,25 @@ include ("./includers/sidebar.php");
 ?>
 
 <div class="content">
-    <div>
+    <!-- <div> -->
 <?php
 include ("./includers/navbar.php");
 
 ?>
+
      <div class="container-fluid pt-4 px-4">
                 <div class="row vh-100 bg-secondary rounded justify-content-center mx-0">
                     <div class="col-md-6 text-center">
                          <h3>Wellcome to admin dashboard</h3> 
                     </div> 
-                 </div>
-    </div> 
+                </div> 
     </div>
-</div>
+<!-- </div> -->
 <?php
 include ("./includers/java.php");
 include("./includers/backtotop.php");
 ?>
+</div>
 </div>
 </body>
 
