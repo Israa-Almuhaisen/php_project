98c1180e55790855926e628e9990ee53d41eff5b
israa muhaisen <israa.muhaisen89@gmail.com>
2023-01-02 20:42:56 +0200

    israaa

--- public_dashboard/__single-product.php
+++ public_dashboard/__single-product.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+include("../admin_dashboard/config.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +11,7 @@
 	<meta name="description" content="Responsive Bootstrap4 Shop Template, Created by Imran Hossain from https://imransdesign.com/">
 
 	<!-- title -->
-	<title>Shop</title>
+	<title>Single Product</title>
 
 	<!-- favicon -->
 	<link rel="shortcut icon" type="image/png" href="../public_dashboard/assets/img/th.jpg">
@@ -35,11 +39,11 @@
 <body>
 	
 	<!--PreLoader-->
-    <div class="loader">
+    <!-- <div class="loader">
         <div class="loader-inner">
             <div class="circle"></div>
         </div>
-    </div>
+    </div> -->
     <!--PreLoader Ends-->
 	
 	<!-- header -->
@@ -51,7 +55,7 @@
 						<!-- logo -->
 						<div class="site-logo">
 							<a href="index.html">
-								<img src="../public_dashboard/assets/img/logomotor (2).png"alt="">
+								<img src="assets/img/logomotor (2).png" alt="">
 							</a>
 						</div>
 						<!-- logo -->
@@ -66,6 +70,7 @@
 									</ul>
 								</li>
 								<li><a href="about.html">About</a></li>
+								
 							
 								<li><a href="contact.html">Contact</a></li>
 								<li><a href="shop.html">Shop</a>
@@ -76,7 +81,7 @@
 										<li><a href="cart.html">Cart</a></li>
 									</ul>
 								</li>
-								<li><a href="../admin_dashboard/signin.html">login</a></li>
+								<li><a href="">login</a></li>
 								<li>
 									<div class="header-icons">
 										<a class="shopping-cart" href="cart.html"><i class="fas fa-shopping-cart"></i></a>
@@ -120,8 +125,8 @@
 			<div class="row">
 				<div class="col-lg-8 offset-lg-2 text-center">
 					<div class="breadcrumb-text">
-						<p>Fresh and Organic</p>
-						<h1>Shop</h1>
+						<p>See more Details</p>
+						<h1>Single Product</h1>
 					</div>
 				</div>
 			</div>
@@ -129,25 +134,137 @@
 	</div>
 	<!-- end breadcrumb section -->
 
-	<!-- products -->
-	<div class="product-section mt-150 mb-150">
+	<!-- single product -->
+	<?php
+	// nnnn = product id sent in url
+	$product_id = $_GET["pid"];
+	// $product_id = 1;
+	$sql = "select * from products where product_id = $product_id";
+	$data= $conn->query($sql);
+	$product = mysqli_fetch_array($data, MYSQLI_ASSOC);
+	// print_r($product);
+	?>
+	<div class="single-product mt-150 mb-150">
 		<div class="container">
-
 			<div class="row">
-                <div class="col-md-12">
-                    <div class="product-filters">
-                        <ul>
-                            <li class="active" data-filter="*">All</li>
-                            <li data-filter=".strawberry">Strawberry</li>
-                            <li data-filter=".berry">Berry</li>
-                            <li data-filter=".lemon">Lemon</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+				<div class="col-md-5">
+					<div class="single-product-img">
+					<?php 
+						$pic=$product["pic_main"];
+						$pic = trim($pic) ;
+						echo "<img style='width: 30em; height:22em' src='../upload/$pic' alt=''>";
+						?>
+					<!-- <img style="width: 30em; height:25em" src='../upload/$pic' alt=''> -->
+					</div>
+				</div>
+				<div class="col-md-7">
+					<div class="single-product-content">
+						<h3>
+							<?php
+							echo $product["product_name"] ;
+							?>
+						</h3>
+						<p class="single-product-pricing"><span></span>
+						<?php
+							echo $product["price"] ;
+							echo " JOD" ;
+							?>
+						</p>
+						<p>
+						<?php
+							echo str_replace("—","<br>",$product["description"]);
+							?>
+						</p>
+						<div class="single-product-form">
+							<form action="" method="POST">
+								<input type="number" name="qun" value="1" min="1" 
+								max="<?php
+								echo $product["in_stock"] ?>">
+								<button type="submit">
+							<p type="submit"><a href="cart.php" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
+								</button>
+							</form>
+							<?php
+							if ($_SERVER['REQUEST_METHOD']=="POST") {
+								
+								$added_product = [$product["product_id"],$_POST["qun"],$product["product_name"],$product["description"],$product["model_year"],$product["brand"],$product["price"],$product["category_id"],$product["pic_main"],$product["rate"],$product["in_stock"],$product["is_discount"],$product["discount"]];
+								if (! isset($_SESSION["added_products"])){
+								$_SESSION["added_products"] = [];
+								}
+								array_push($_SESSION["added_products"],$added_product);
+								print_r($_SESSION["added_products"]);
+							}
+							?>
+							<p><strong>Categories: </strong>	
+						
+							<?php
+							$cat_id = $product["category_id"];
+							$sql = "select * from categories where category_id = $cat_id";
+							$cat= $conn->query($sql);
+							$cat = mysqli_fetch_array($cat, MYSQLI_ASSOC);
+							echo $cat["category_name"];
+							$cat_id=$cat["category_id"]
+							?>							
+							</p>
+						</div>
+						<!-- <h4>Share:</h4>
+						<ul class="product-share">
+							<li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href=""><i class="fab fa-twitter"></i></a></li>
+							<li><a href=""><i class="fab fa-google-plus-g"></i></a></li>
+							<li><a href=""><i class="fab fa-linkedin"></i></a></li>
+						</ul> -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end single product -->
 
-			<div class="row product-lists">
-				<div class="col-lg-4 col-md-6 text-center strawberry">
+	<!-- more products -->
+	<div class="more-products mb-150">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 text-center">
+					<div class="section-title">	
+						<h3><span class="orange-text">Related</span> Products</h3>
+						<!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, fuga quas itaque eveniet beatae optio.</p> -->
+					</div>
+				</div>
+			</div>
+			<div class="row">
+			<?php
+							// session_start();
+
+							include("../admin_dashboard/config.php");
+
+							$sql = "select * from products where category_id=$cat_id";
+							$data= $conn->query($sql);
+				foreach($data as $ele){
+					if ($ele["is_discount"]){
+						echo '<div class="col-lg-4 col-md-6 text-center">
+						<div class="single-product-item">
+						<div class="product-image">';
+						$pic=$ele["pic_main"];
+						$id=$ele["product_id"];
+						// echo $pic;
+						echo "<a href='single-product.php?product_id=$id'><img src='../admin_dashboard/img/products/$pic' alt=''></a>";
+						echo "</div>";
+						$name = $ele['product_name'];
+						echo "<h3>$name</h3>";
+						$old_price = $ele['price'];
+						$new_price =$old_price - ($ele['discount'] * $old_price)/100;
+						echo "<h3> <span class='product-price'><del>$old_price </del></span><span class='product-price'>$$new_price</span></h3>";
+						echo "<a href='add_to_cart.php?productid=$id' class='cart-btn'><i class='fas fa-shopping-cart'></i> Add to Cart</a></div></div>";
+						// $url = getcwd();
+						// $url = (parse_url($url, PHP_URL_FRAGMENT));
+						// $url =str_replace('C:\xampp\htdocs',"localhost",$url);
+						// echo $url;
+						// $_SESSION["current_url"]= $url;
+					}
+				}
+				?>
+				<!-- <div class="col-lg-4 col-md-6 text-center">
 					<div class="single-product-item">
 						<div class="product-image">
 							<a href="single-product.html"><img src="assets/img/products/product-img-1.jpg" alt=""></a>
@@ -156,8 +273,8 @@
 						<p class="product-price"><span>Per Kg</span> 85$ </p>
 						<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
 					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 text-center berry">
+				</div> -->
+				<!-- <div class="col-lg-4 col-md-6 text-center">
 					<div class="single-product-item">
 						<div class="product-image">
 							<a href="single-product.html"><img src="assets/img/products/product-img-2.jpg" alt=""></a>
@@ -167,7 +284,7 @@
 						<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
 					</div>
 				</div>
-				<div class="col-lg-4 col-md-6 text-center lemon">
+				<div class="col-lg-4 col-md-6 offset-lg-0 offset-md-3 text-center">
 					<div class="single-product-item">
 						<div class="product-image">
 							<a href="single-product.html"><img src="assets/img/products/product-img-3.jpg" alt=""></a>
@@ -175,56 +292,12 @@
 						<h3>Lemon</h3>
 						<p class="product-price"><span>Per Kg</span> 35$ </p>
 						<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 text-center">
-					<div class="single-product-item">
-						<div class="product-image">
-							<a href="single-product.html"><img src="assets/img/products/product-img-4.jpg" alt=""></a>
-						</div>
-						<h3>Avocado</h3>
-						<p class="product-price"><span>Per Kg</span> 50$ </p>
-						<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 text-center">
-					<div class="single-product-item">
-						<div class="product-image">
-							<a href="single-product.html"><img src="assets/img/products/product-img-5.jpg" alt=""></a>
-						</div>
-						<h3>Green Apple</h3>
-						<p class="product-price"><span>Per Kg</span> 45$ </p>
-						<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 text-center strawberry">
-					<div class="single-product-item">
-						<div class="product-image">
-							<a href="single-product.html"><img src="assets/img/products/product-img-6.jpg" alt=""></a>
-						</div>
-						<h3>Strawberry</h3>
-						<p class="product-price"><span>Per Kg</span> 80$ </p>
-						<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
-					</div>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col-lg-12 text-center">
-					<div class="pagination-wrap">
-						<ul>
-							<li><a href="#">Prev</a></li>
-							<li><a href="#">1</a></li>
-							<li><a class="active" href="#">2</a></li>
-							<li><a href="#">3</a></li>
-							<li><a href="#">Next</a></li>
-						</ul>
-					</div>
+					</div> -->
 				</div>
 			</div>
 		</div>
 	</div>
-	<!-- end products -->
+	<!-- end more products -->
 
 	<!-- logo carousel -->
 	<div class="logo-carousel-section">
@@ -233,22 +306,19 @@
 				<div class="col-lg-12">
 					<div class="logo-carousel-inner">
 						<div class="single-logo-item">
-							<img src="../public_dashboard/assets/img/company-logos/R (1).png" alt="">
+							<img src="assets/img/company-logos/1.png" alt="">
 						</div>
 						<div class="single-logo-item">
-							<img src="../public_dashboard/assets/img/company-logos/bmw.png" alt="">
+							<img src="assets/img/company-logos/2.png" alt="">
 						</div>
 						<div class="single-logo-item">
-							<img src="../public_dashboard/assets/img/company-logos/honda.png" alt="">
+							<img src="assets/img/company-logos/3.png" alt="">
 						</div>
 						<div class="single-logo-item">
-							<img src="../public_dashboard/assets//img/company-logos/kkk.png" alt="">
+							<img src="assets/img/company-logos/4.png" alt="">
 						</div>
 						<div class="single-logo-item">
-							<img src="../public_dashboard/assets/img/company-logos//ktm.png" alt="">
-						</div>
-						<div class="single-logo-item">
-							<img src="../public_dashboard/assets/img/company-logos/Suzuki.png" alt="">
+							<img src="assets/img/company-logos/5.png" alt="">
 						</div>
 					</div>
 				</div>
@@ -257,6 +327,8 @@
 	</div>
 	<!-- end logo carousel -->
 
+
+	
 	<!-- copyright -->
 	<div class="copyright">
 		<div class="container">
